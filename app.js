c983a0d049568c5de40dd98791635b862596fd07
Kartik-2007-code <magarkartik03@gmail.com>
2025-11-17 22:16:58 +0530

    app

--- app.js
+++ app.js
@@ -3,31 +3,29 @@ const mongoose = require("mongoose");
 const path = require("path");
 const bcrypt = require("bcryptjs");
 const session = require("express-session");
-// local module
+
+// local modules
 const loginRouter = require("./routes/loginRouter");
 const registerRouter = require("./routes/registerRouter");
 const noteRoute = require("./routes/notesRouter");
 const logoutRoute = require("./routes/logoutRouter");
-// const initializePassword = require('./password-config');
-app.get("/", (req, res) => {
-  res.redirect("/login");
-});
+
 const app = express();
+
 const uri =
   "mongodb+srv://root:root@completecoding.kqe40.mongodb.net/notesdb?retryWrites=true&w=majority";
 
+// MONGO CONNECTION
 mongoose
   .connect(uri, {})
-  .then(() => {
-    console.log("MogoDb is connected successfully");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  .then(() => console.log("MongoDb is connected successfully"))
+  .catch((err) => console.log(err));
 
+// VIEW ENGINE
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// MIDDLEWARES
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
@@ -37,14 +35,19 @@ app.use(
   })
 );
 
+// ✅ ADD THIS HOME ROUTE HERE
+app.get("/", (req, res) => {
+  return res.redirect("/login"); // or /register or /notes
+});
+
+// ROUTES
 app.use(logoutRoute);
 app.use(registerRouter);
 app.use(loginRouter);
 app.use(noteRoute);
 
-const PORT = 3000;
+// SERVER
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(
-    `Server is running on the address http://localhost:${PORT}/register`
-  );
+  console.log(`Server running at port ${PORT}`);
 });
