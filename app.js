746bb6ab6bb1a5f6287cee373111a85ef8a18ca9
Kartik-2007-code <magarkartik03@gmail.com>
2025-11-17 21:27:50 +0530

    Deploying changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypts");
 const session = require("express-session");
 // local module
 const loginRouter = require("./routes/loginRouter");
